a7bf082c3de4b641204cea97db9c607f01a38544
Max M <meshstyles@users.noreply.github.com>
2020-06-08 10:42:48 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
 // disable @everyone for this bot instance
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({ disableEveryone: true });
 
-bot.on("ready", async() => {
-  //resoponse to see bot is alive
-  console.log(`Bot is online`);
-  //activty
-  bot.user.setActivity("I'm a Bot lol")
+bot.on("ready", async () => {
+    //resoponse to see bot is alive
+    console.log(`Bot is online`);
+    //activty
+    bot.user.setActivity("I'm a Bot lol")
 });
 
 /**
@@ -17,10 +17,23 @@ bot.on("ready", async() => {
  * @returns void
  */
 bot.on("message", async message => {
-  if(message.content.startsWith("!ping")){
-    message.reply("pong");
-  } 
+    if (message.author.bot)
+        return;
+
+    // fyi only last 14days of messages can be deleted
+    if (message.content.startsWith("!clear")) {
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            channelcleaner(message);
+        }
+    }
+
+
 });
+
+async function channelcleaner(message) {
+    var rmList = await message.channel.fetchMessages();
+    message.channel.bulkDelete(rmList);
+}
 
 //login bot into to discord api with auth token
 bot.login(botconfig.token);
